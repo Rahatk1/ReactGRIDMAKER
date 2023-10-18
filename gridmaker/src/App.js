1fed3e19bace51adefb9428b7b094ba2464cde04
Kazi Anwar <kazisameen2702@gmail.com>
2023-10-18 12:32:13 -0400

    testing if changes on addrow get tracked to app.js

--- gridmaker/src/App.js
+++ gridmaker/src/App.js
@@ -1,3 +1,13 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// export default function Square() {
+//   return <button className="square">z</button>;
+// }
+
+import Square from './addRow'; // Ensure the correct path
+
+export default function App() {
+  return (
+    <div className="App">
+      <Square /> {/* Render the Circle component */}
+    </div>
+  );
 }
