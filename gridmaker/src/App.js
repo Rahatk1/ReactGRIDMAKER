1891a11fc379d82d3ac8ad8962067dad44bca0a3
Kazi Anwar <kazisameen2702@gmail.com>
2023-10-20 02:00:37 -0400

    attempting addRow.js

--- gridmaker/src/App.js
+++ gridmaker/src/App.js
@@ -2,12 +2,28 @@
 //   return <button className="square">z</button>;
 // }
 
-import Square from './addRow'; // Ensure the correct path
+import React, { useState } from 'react'; ///imports modules needed fromn react and
+import AddRow from './addRow'; // import AddRow
 
-export default function App() {
+function App() {
+  const [grid, setGrid] = useState([[]]); //empty grid? empty array?
+
   return (
     <div className="App">
-      <Square /> {/* Render the Circle component */}
+      <AddRow grid={grid} setGrid={setGrid} /> {/* passes grid and setGrid to AddRow so u can add*/}
+      <table>
+        <tbody>
+          {grid.map((row, rowIndex) => ( //loop thru the rwos and then tr is new row 
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => ( //loop thru cells in that row
+                <td key={cellIndex}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
+
+export default App;
