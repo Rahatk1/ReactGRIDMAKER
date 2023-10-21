823b274f7362559202044f59f8be29af9f0089bb
DavidA123777 <ddda4167@gmail.com>
2023-10-20 22:20:35 -0400

    created the implementation of addRow in addRow.js and updated the table layout and function for addRow inside of App.js. The style for the table was also updated to be a more suitable square design inside of styles.css

--- gridmaker/src/App.js
+++ gridmaker/src/App.js
@@ -1,27 +1,58 @@
-// export default function Square() {
-//   return <button className="square">z</button>;
+// import React, { useState } from 'react'; ///imports modules needed fromn react and
+// import AddRow from './addRow'; // import AddRow
+
+// function App() {
+//   const [grid, setGrid] = useState([[]]); //empty grid? empty array?
+
+//   return (
+//     <div className="App">
+//       <AddRow grid={grid} setGrid={setGrid} /> {/* passes grid and setGrid to AddRow so u can add*/}
+//       <table>
+//         <tbody>
+//           {grid.map((row, rowIndex) => ( //loop thru the rwos and then tr is new row 
+//             <tr key={rowIndex}>
+//               {row.map((cell, cellIndex) => ( //loop thru cells in that row
+//                 <td key={cellIndex}>{cell}</td>
+//               ))}
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
 // }
 
-import React, { useState } from 'react'; ///imports modules needed fromn react and
-import AddRow from './addRow'; // import AddRow
+
+// export default App;
+
+
+import React, { useState } from 'react';
+import './styles.css'; // Import the CSS file
+import AddRow from './addRow'; // Import the AddRow component
 
 function App() {
-  const [grid, setGrid] = useState([[]]); //empty grid? empty array?
+  const [tableData, setTableData] = useState([]);
 
+  const handleAddRow = () => {
+    setTableData([...tableData, ['square']]);
+  };
+
+
+  
   return (
-    <div className="App">
-      <AddRow grid={grid} setGrid={setGrid} /> {/* passes grid and setGrid to AddRow so u can add*/}
-      <table>
+    <div>
+      <table className="table">
         <tbody>
-          {grid.map((row, rowIndex) => ( //loop thru the rwos and then tr is new row 
+          {tableData.map((row, rowIndex) => (
             <tr key={rowIndex}>
-              {row.map((cell, cellIndex) => ( //loop thru cells in that row
-                <td key={cellIndex}>{cell}</td>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex} className={cell}></td>
               ))}
             </tr>
           ))}
         </tbody>
       </table>
+      <AddRow onAddRow={handleAddRow} />
     </div>
   );
 }
--- gridmaker/src/addRow.js
+++ gridmaker/src/addRow.js
@@ -1,18 +1,32 @@
+// import React from 'react';
+
+// function AddRow({ grid, setGrid }) //function called when add row button is clicked
+// {
+//   const addRow = () => 
+//   {
+//     //create new row full of null values, num of cells = num of cells in the row above, or if no row above its set to 1
+//     const newRow = Array(grid[0] ? grid[0].length : 1).fill(null); //new row created witht he same number of cells as the row before it
+//     setGrid([...grid, newRow]); //updates the grid w new rows?
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={addRow}>Add Row</button> 
+//     </div> //makes button element
+//   );
+// }
+
+// export default AddRow;
+
+
+
 import React from 'react';
 
-function AddRow({ grid, setGrid }) //function called when add row button is clicked
-{
-  const addRow = () => 
-  {
-    //create new row full of null values, num of cells = num of cells in the row above, or if no row above its set to 1
-    const newRow = Array(grid[0] ? grid[0].length : 1).fill(null); //new row created witht he same number of cells as the row before it
-    setGrid([...grid, newRow]); //updates the grid w new rows?
-  };
-
+function AddRow({ onAddRow }) {
   return (
-    <div>
-      <button onClick={addRow}>Add Row</button> 
-    </div> //makes button element
+    <div className="add-row">
+      <button onClick={onAddRow}>Add Row</button>
+    </div>
   );
 }
 
