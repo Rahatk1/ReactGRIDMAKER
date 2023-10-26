04f39129a2a9e605c72c102ceb23cfa8cb8486e5
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-10-26 00:04:00 -0400

    added a colorwheel replacing the dropdown menu

--- gridmaker/src/selectColor.js
+++ gridmaker/src/selectColor.js
@@ -3,12 +3,7 @@ import React from "react";
 function SelectColor() {
     return (
         <div>
-            <select id="colorPicker">
-            <option value="select">SELECT</option>
-            <option value="red">Red</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
-        </select>
+             <input type="color" id="colorPicker"></input>
         </div>
     )
 }
