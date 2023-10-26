6842639df825b5c492a57c6a71778f1dbf756578
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-10-26 18:36:53 -0400

    removed console log and renamed file to be more appropriate to task at hand

--- gridmaker/src/retrieveColor.js
+++ gridmaker/src/retrieveColor.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SelectColor(props) {
+function RetrieveColor(props) {
 
     return (
         <div>
@@ -9,4 +9,4 @@ function SelectColor(props) {
     )
 }
 
-export default SelectColor;
+export default RetrieveColor;
