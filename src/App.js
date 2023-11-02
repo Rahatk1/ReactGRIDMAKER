56bdfec889f1c31e3fab6462c69f016f0edc2072
DavidA123777 <ddda4167@gmail.com>
2023-11-02 15:04:52 -0400

    implemented surroundCell feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // Import the necessary stuff and below the components from teh files
+import React, { useState } from 'react';
 import './styles.css';
 import AddRow from './addRow';
 import AddCol from './addCol';
@@ -6,68 +6,37 @@ import RemoveRow from './removeRow';
 import RemoveCol from './removeCol';
 import SelectColor from './selectColor';
 import FillAllCells from './FillAllCells';
-import RemoveColor from './RemoveColor'; // Import the new component
+import RemoveColor from './RemoveColor';
 import FillAllUncolored from './FillAllUncolored';
+import debounce from 'lodash/debounce'; // Import debounce
 
-function App() 
-{
-  const [tableData, setTableData] = useState([]); // a state variable tableData (initialized as empty array)
-
-  // i think i should use state for the color selection
+function App() {
+  const [tableData, setTableData] = useState([]);
   const [colorChoice, setColor] = useState("#FFFFFF");
-
-  // const handleAddRow = () => //called when addrow is clicked, same explanation for the rest so I won't repeat
-  // { 
-  //   if (tableData.length === 0) //ADDED THIS ONE TODAY - this checks if tabledata is empty like there are no rows yet
-  //   { 
-  //     setTableData([['square']]);
-  //   } 
-    
-  //   else //if ur here that means there ARE rows in the grid
-  //   {
-  //     setTableData([...tableData, Array(tableData[0].length).fill('square')]); 
-  //     //tableData[0].length gets number of cells int he first row of the grid
-  //     //then blah blah.fill('square') creates array with same amount of cells as the first row & fills them all with 'square'
-  //   }
-  // };
-
-  // const handleAddCol = () => 
-  // {
-  //   const newTableData = tableData.map(row => [...row, 'square']); // new array where ur adding a 'square' value to each row aka a column
-  //   setTableData(newTableData); //Update tableData state so it now includes the new column
-  // };
-
+  const [fillMode, setFillMode] = useState("default");
+  
   const handleAddRow = () => {
     if (tableData.length === 0) {
-      // If there are no rows, add a new row with a single column
       setTableData([['square']]);
     } else {
-      // Add a new row with the same number of columns as the previous row
       const newRow = Array(tableData[0].length).fill('square');
       setTableData([...tableData, newRow]);
     }
   };
-  
 
-
-  
   const handleAddCol = () => {
     if (tableData.length === 0) {
-      // If there are no rows, add a new column and a new row
       setTableData([['square']]);
     } else {
-      // Add a new column to each existing row
       const newTableData = tableData.map(row => [...row, 'square']);
       setTableData(newTableData);
     }
   };
 
-  const handleRemoveRow = () => 
-  {
-    if (tableData.length > 0) //checks if there are any rows in the grid to remove in the first place, if there are then:
-    {
-      const newTableData = tableData.slice(0, -1); //slice(0, -1) removes the last item aka the last row) from the array and puts it in newTableData
-      setTableData(newTableData); //this updates state of tabledata with new array (newTableData) which now has the last row remoed
+  const handleRemoveRow = () => {
+    if (tableData.length > 0) {
+      const newTableData = tableData.slice(0, -1);
+      setTableData(newTableData);
     }
   };
 
@@ -76,83 +45,131 @@ function App()
       const newTableData = tableData.map(row => [...row.slice(0, -1)]);
       setTableData(newTableData);
     } else {
-      // Handle the case when there are no columns left
       setTableData([]);
     }
   };
+
   const handleSelectColor = (event) => {
     setColor(event.target.value);
   };
 
-  // Color in all cells in the grid
   const handleFillAllCells = () => {
-    const newTableData = tableData.map(row => row.map(cell => ({ ...cell, color: colorChoice }))); // This row basically updates the color of all cells in the table to the currently selected
-    setTableData(newTableData); // Update the state component
+    const newTableData = tableData.map(row => row.map(cell => ({ ...cell, color: colorChoice })));
+    setTableData(newTableData);
   };
 
-  // handler func to render user color choice to an click event
+
+
+
+
+
   const handleCellClick = (clickedRow, clickedCell) => {
-    const updatedTableData = tableData.map((row, rowIndex) =>
-      rowIndex === clickedRow ? 
-        row.map((cell, cellIndex) =>
-          cellIndex === clickedCell ? { ...cell, color: colorChoice } : cell
-        ) : row
-    );
+    const targetColor = tableData[clickedRow][clickedCell].color;
+    let updatedTableData = [...tableData];
+  
+    if (fillMode === 'default') {
+      // In "default" mode, change the color of the clicked cell only
+      updatedTableData[clickedRow] = [...updatedTableData[clickedRow]];
+      updatedTableData[clickedRow][clickedCell] = { ...updatedTableData[clickedRow][clickedCell], color: colorChoice };
+    } else if (fillMode === 'fill') {
+      // In "fill" mode, fill adjacent cells with the same color using an iterative approach
+  
+      const stack = [[clickedRow, clickedCell]];
+      const visited = new Set();
+  
+      while (stack.length > 0) {
+        const [row, cell] = stack.pop();
+        if (visited.has(`${row}-${cell}`)) continue;
+        visited.add(`${row}-${cell}`);
+  
+        if (
+          row >= 0 &&
+          cell >= 0 &&
+          row < updatedTableData.length &&
+          cell < updatedTableData[0].length &&
+          updatedTableData[row][cell].color === targetColor
+        ) {
+          updatedTableData[row] = [...updatedTableData[row]];
+          updatedTableData[row][cell] = { ...updatedTableData[row][cell], color: colorChoice };
+  
+          stack.push([row + 1, cell]);
+          stack.push([row - 1, cell]);
+          stack.push([row, cell + 1]);
+          stack.push([row, cell - 1]);
+        }
+      }
+    }
+  
     setTableData(updatedTableData);
   };
   
-  const handleFillAllUncoloredCells = () => 
-  {
-    const newTableData = tableData.map((row) => //iterate thru each row
-      row.map((cell) => //in each row iterate thru each cell
-      {
-        if (!cell.color || cell.color === "#FFFFFF") //the ! made me feel so smart fr but anyways this is like if the cell color is null or not there aka uncolored
-        //update to above condition, rahats remove color updates cell colors to white so fill uncolor should now also be able to color in cells that are white now too
-        {
-          return { ...cell, color: colorChoice }; //update color based on colorchocie
+  
+  
+  
+  
+  
+
+  const handleFillAllUncoloredCells = () => {
+    const newTableData = tableData.map((row) =>
+      row.map((cell) => {
+        if (!cell.color || cell.color === "#FFFFFF") {
+          return { ...cell, color: colorChoice };
         }
         return cell;
       })
     );
-    setTableData(newTableData); //update as per usual
-  };  
-  
-  //Removes all color
-  const handleRemoveColor = () => {
-    const clearedTableData = tableData.map(row => row.map(cell => ({ ...cell, color: '#FFFFFF' }))); // Set all cell colors to white
-    setTableData(clearedTableData); //update
+    setTableData(newTableData);
   };
+
+  const handleRemoveColor = () => {
+    const clearedTableData = tableData.map(row =>
+      row.map(cell => ({ ...cell, color: '#FFFFFF' }))
+    );
+    setTableData(clearedTableData);
+  };
+
+  const handleToggleFillMode = () => {
+    setFillMode(fillMode === 'default' ? 'fill' : 'default');
+  };
+
+  const debouncedHandleCellClick = debounce(handleCellClick, 100); // Adjust the delay as needed
+
 
   return (
     <>
-    <h2>Assignment#4 React Gridmaker</h2>
-    <h4>Group: Lally Enthusiasts</h4>
-    <div className="container">
+      <h2>Assignment#4 React Gridmaker</h2>
+      <h4>Group: Lally Enthusiasts</h4>
+      <div className="container">
         <AddRow onAddRow={handleAddRow} />
         <AddCol onAddCol={handleAddCol} />
         <RemoveRow onRemoveRow={handleRemoveRow} />
         <RemoveCol onRemoveCol={handleRemoveCol} />
         <SelectColor value={colorChoice} onChange={handleSelectColor} />
         <FillAllCells onFillAllCells={handleFillAllCells} />
-        <RemoveColor onRemoveAllColors={handleRemoveColor} />      
+        <RemoveColor onRemoveAllColors={handleRemoveColor} />
         <FillAllUncolored onFillAllUncoloredCells={handleFillAllUncoloredCells} />
-    </div>
-      
+        <button onClick={handleToggleFillMode}>
+          Toggle Fill Mode: {fillMode === 'default' ? 'Default' : 'Fill'}
+        </button>
+      </div>
+
       <table className="table">
-      <tbody>
-        {tableData.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <td key={cellIndex} style={{ border: '1px solid black', backgroundColor: cell.color, width: '34px', height: '34px' }}
-              onClick={() => handleCellClick(rowIndex, cellIndex)}></td>
-            ))}
-          </tr>
-        ))}
+        <tbody>
+          {tableData.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td
+                  key={cellIndex}
+                  style={{ border: '1px solid black', backgroundColor: cell.color, width: '34px', height: '34px' }}
+                  onClick={() => debouncedHandleCellClick(rowIndex, cellIndex)} // Use the debounced function
+                ></td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
-
   );
 }
 
-export default App; // Exportapp comp
+export default App;
